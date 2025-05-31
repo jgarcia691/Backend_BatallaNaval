e2db0d605aef5c9ebf7f48a32617c80624a1b3da
Jenorg <jenfermz44@gmail.com>
2025-05-30 19:32:39 -0500

    fix: ruta de gameManager actualizada

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors'; // Importa el paquete cors
-import { GameManager } from './classes/multiplayer/gameManager.js';
+import { GameManager } from '../classes/multiplayer/gameManager.js'; // Asegúrate de que la ruta sea correcta
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(cors({
 const server = createServer(app);
 const io = new Server(server, {
   cors: { // Mantén también la configuración de Socket.IO por si acaso
-    origin: 'https://batalla-naval-navy.vercel.app',
+    origin: ['https://batalla-naval-navy.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true
